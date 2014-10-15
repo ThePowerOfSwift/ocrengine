710e85d359b5826a545173257c294831562abae4
uttam <uttam@IMCHDT017>
2014-10-15 17:34:30 +0530

    added standard images set in src/main/resources/images

--- src/main/java/com/imaginea/api/ImageProcessor.java
+++ src/main/java/com/imaginea/api/ImageProcessor.java
@@ -41,7 +41,7 @@ import com.imaginea.process.OtsuBinarize;
 public class ImageProcessor {
 
 	private static final Logger logger = Logger.getLogger(ImageProcessor.class);
-	private static final String img_dir_path = "src/test/resources/ima/goodImages/";
+	private static final String img_dir_path = "src/main/resources/Images/";
 
 	public static Map<String, Map<String, String>> benchmark() {
 		logger.info("Reading images");
@@ -171,7 +171,7 @@ public class ImageProcessor {
 
 			try {
 
-				String result = instance.doOCR(imageFile);
+				String result = instance.doOCR(binaryFile);
 				String[] results = result.split("\n");
 				int i = 0;
 				Map<String, String> licenseInfo = new HashMap<>();
