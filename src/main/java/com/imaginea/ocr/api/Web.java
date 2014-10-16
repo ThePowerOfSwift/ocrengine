ddee13e18345b35b0070673bbe6e2dfc169f35d2
milli <gowrishankar.milli@gmail.com>
2014-10-16 21:12:36 +0530

    cleanup Updates

--- src/main/java/com/imaginea/ocr/api/Web.java
+++ src/main/java/com/imaginea/ocr/api/Web.java
@@ -9,6 +9,7 @@ import static spark.SparkBase.staticFileLocation;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
@@ -22,7 +23,9 @@ import org.apache.log4j.Logger;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import com.imaginea.api.ImageProcessor;
 import com.imaginea.ocr.Benchmark;
+import com.imaginea.ocr.OCR;
 import com.imaginea.ocr.Props;
 
 public class Web {
@@ -31,72 +34,83 @@ public class Web {
 
 	// Constants
 	private static final String MULTIPART_CONFIG = "org.eclipse.multipartConfig";
-	
-	
+
 	public static void main(String args[]) {
 
 		setPort(WEB_PORT);
 		staticFileLocation(STATIC_FILE_LOC);
 
 		/* --- Web API End Points --- */
-		
+
 		// process a given file
 		post("/process", (req, res) -> {
 			logger.info("Web request : /process");
-			
-			MultipartConfigElement multipartConfigElement = new MultipartConfigElement(Props.TEMP_DIR);
 
-			req.raw().setAttribute(MULTIPART_CONFIG, multipartConfigElement);
 			try {
-				Part file = req.raw().getPart("file");
+				MultipartConfigElement multipartConfigElement = new MultipartConfigElement(Props.TEMP_DIR);
+				req.raw().setAttribute(MULTIPART_CONFIG, multipartConfigElement);
 
-				InputStream initialStream = file.getInputStream();
+				Part part = req.raw().getPart("file");
+				File imFile = extractFile(part);
 
-				File targetFile = new File("src/main/resources/targetFile.jpg");
-				OutputStream outStream = new FileOutputStream(targetFile);
+				// Process the file
+				return OCR.process(imFile);
 
-				byte[] buffer = new byte[8 * 1024];
-				int bytesRead;
-				while ((bytesRead = initialStream.read(buffer)) != -1) {
-					outStream.write(buffer, 0, bytesRead);
-				}
-
-				outStream.close();
-
-//				return process(targetFile);
-				return null;
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
+				logger.error(e.getMessage());
 				e.printStackTrace();
 			}
 
-			//
-				return "";
+			return null;
 
-			}
+		});
 
-		);
-		
-		// Benchmark results 
+		// Benchmark results
 		get("/benchmark", (request, response) -> {
 			logger.info("Web request : /benchmark");
-			
+
 			Map<String, Object> attrs = new HashMap<>();
 			attrs.put("attrs", Benchmark.standardImagesSet());
-	
+
 			return new ModelAndView(attrs, "benchmark.html");
 		}, new FreeMarkerEngine());
-		
+
 		// Health Check
 		get("/test", (request, response) -> {
 			logger.info("Web request : /test");
-			
+
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("message", "Hello World!");
-			
+
 			return new ModelAndView(attributes, "index.html");
 		}, new FreeMarkerEngine());
 
+	}
+
+	/**
+	 * Extracts file file from "Part"
+	 */
+	private static File extractFile(Part file) {
+		File targetFile = null;
+		try {
+			String tempFileName = "temp-" + System.currentTimeMillis() + ".jpg";
+			targetFile = new File(tempFileName);
+
+			InputStream initialStream = file.getInputStream();
+			OutputStream outStream = new FileOutputStream(targetFile);
+
+			byte[] buffer = new byte[8 * 1024];
+			int bytesRead;
+			while ((bytesRead = initialStream.read(buffer)) != -1) {
+				outStream.write(buffer, 0, bytesRead);
+			}
+
+			outStream.close();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+
+		return targetFile;
 	}
 
 }
